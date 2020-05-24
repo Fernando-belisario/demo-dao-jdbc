eebdfb197ecdd102e8f7d44b437ab9fdb19c52a0
fernando-belisario <fernandoblz20@hotmail.com>
2020-05-23 21:02:42 -0300

    department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import model.entities.Departament;
+
+/**
+ *
+ * @author ferna
+ */
+public class Program {
+    public static void main(String[] args) {
+        Departament obj = new Departament(1, "Books");
+        System.out.println(obj);
+                
+    }
+    
+}
